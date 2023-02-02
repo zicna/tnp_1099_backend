309de4c44fe03b45148f9ed2ab6daeca54029c17
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-02 11:49:14 -0600

    first commit (boiler code with configuring of application)

---
+++ src/main/java/com/zicna_inc/tnp_1099/Tnp1099Application.java
@@ -0,0 +1,13 @@
+package com.zicna_inc.tnp_1099;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tnp1099Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tnp1099Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zicna_inc/tnp_1099/Tnp1099ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zicna_inc.tnp_1099;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tnp1099ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
