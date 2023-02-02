cebebe21ce2cf7911d1ad08b2ea4b04fb9ef4117
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-02 14:33:47 -0600

    add User POJO, add Gender Enum (used in User as field type)

---
+++ src/main/java/com/zicna_inc/tnp_1099/entity/User.java
@@ -0,0 +1,73 @@
+package com.zicna_inc.tnp_1099.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column
+    private String email;
+
+    @Column
+    private Date date_of_birth;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+
+
+
+    public User(Long id, String email, Date date_of_birth, Gender gender) {
+        this.id = id;
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+        this.gender = gender;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate_of_birth() {
+        return this.date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+}
