8b5b6ae664bdeaa90d824955d1aef471d14a862f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-02 14:17:05 -0600

    add TripController with getTrip and saveTrip methods

---
+++ src/main/java/com/zicna_inc/tnp_1099/web/TripController.java
@@ -0,0 +1,34 @@
+package com.zicna_inc.tnp_1099.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zicna_inc.tnp_1099.entity.Trip;
+import com.zicna_inc.tnp_1099.service.TripService;
+
+@RestController
+public class TripController {
+
+    @Autowired
+    TripService tripService;
+
+
+    @GetMapping("/trip/{id}")
+    public ResponseEntity<Trip> getTrip(@PathVariable Long id){
+        Trip trip = tripService.getTrip(id);
+        return new ResponseEntity<>(trip, HttpStatus.OK);
+    }
+
+    @PostMapping("/trip")
+    public ResponseEntity<Trip> addTrip(@RequestBody Trip trip){
+        return new ResponseEntity<>(tripService.saveTrip(trip), HttpStatus.CREATED);
+    }
+    
+}
