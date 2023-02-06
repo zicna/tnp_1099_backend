addc4b6ed51fbce995fc76fff7a201575c40325e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-06 13:48:16 -0600

    implementation of CrudRepository class lives as a bean and @Repository redundant

--- src/main/java/com/zicna_inc/tnp_1099/repository/TripRepository.java
+++ src/main/java/com/zicna_inc/tnp_1099/repository/TripRepository.java
@@ -1,12 +1,10 @@
 package com.zicna_inc.tnp_1099.repository;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.zicna_inc.tnp_1099.entity.Trip;
 import java.lang.Long;
 
 
-@Repository
 public interface TripRepository extends CrudRepository<Trip, Long>{
     
 }
