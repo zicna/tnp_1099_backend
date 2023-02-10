e850956380f3b87b469e20cdeb5d876142c01a6e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-10 15:55:05 -0600

    add custom query for Crud Repo

--- src/main/java/com/zicna_inc/tnp_1099/repository/ExpenseRepository.java
+++ src/main/java/com/zicna_inc/tnp_1099/repository/ExpenseRepository.java
@@ -1,8 +1,11 @@
 package com.zicna_inc.tnp_1099.repository;
 
+import java.util.*;
+
 import com.zicna_inc.tnp_1099.entity.Expense;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExpenseRepository extends CrudRepository<Expense, Long>{
+    List<Expense> findByUserId(Long user_id);
 }
