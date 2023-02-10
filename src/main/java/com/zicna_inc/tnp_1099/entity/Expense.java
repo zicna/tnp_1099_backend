8a17bc40147fc5f7b69314cdf9eb2ee2a544b34f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-10 13:44:26 -0600

    add new entity Expense and enum used as field type in Expense

---
+++ src/main/java/com/zicna_inc/tnp_1099/entity/Expense.java
@@ -0,0 +1,101 @@
+package com.zicna_inc.tnp_1099.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    //* private Long user_id;
+
+    @Enumerated(value=EnumType.STRING)
+    private ExpenseType type;
+    @Column
+    private Double amount;
+    @Column
+    private Date date;
+    @Column
+    private String description;
+
+
+    public Expense() {
+    }
+
+    public Expense(Long id, ExpenseType type, Double amount, Date date, String description) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ExpenseType getType() {
+        return this.type;
+    }
+
+    public void setType(ExpenseType type) {
+        this.type = type;
+    }
+
+    public Double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Expense id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public Expense type(ExpenseType type) {
+        setType(type);
+        return this;
+    }
+
+    public Expense amount(Double amount) {
+        setAmount(amount);
+        return this;
+    }
+
+    public Expense date(Date date) {
+        setDate(date);
+        return this;
+    }
+
+}
---
+++ src/main/java/com/zicna_inc/tnp_1099/entity/ExpenseType.java
@@ -0,0 +1,5 @@
+package com.zicna_inc.tnp_1099.entity;
+
+public enum ExpenseType {
+    GAS, WASH, TOLL, REAPAIR, MISCELLANEOUS, TICKET
+}
