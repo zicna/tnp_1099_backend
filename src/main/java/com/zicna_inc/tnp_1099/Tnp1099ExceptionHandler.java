14dd8bf12de61fa5f340a9f2312fc5bdddb71af1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-11 18:02:43 -0600

    handle NoExpenseException if thrown

--- src/main/java/com/zicna_inc/tnp_1099/Tnp1099ExceptionHandler.java
+++ src/main/java/com/zicna_inc/tnp_1099/Tnp1099ExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.zicna_inc.tnp_1099.exceptions.NoUserException;
 import com.zicna_inc.tnp_1099.exceptions.NoRiderException;
+import com.zicna_inc.tnp_1099.exceptions.NoExpenseException;
 import com.zicna_inc.tnp_1099.exceptions.ErrorResponse;
 
 @ControllerAdvice
@@ -20,6 +21,12 @@ public class Tnp1099ExceptionHandler {
 
     @ExceptionHandler(NoRiderException.class)
     public ResponseEntity<Object> noRiderFound(NoRiderException ex){
+        ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoExpenseException.class)
+    public ResponseEntity<Object> noExpenseFound(NoExpenseException ex){
         ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
