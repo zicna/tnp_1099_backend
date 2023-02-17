5f319282074bb7701e8c40bdb59a2e377cc4f98b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-02-17 15:14:26 -0600

    implementation of saveTrip option two (with dummy rider created every time)

--- src/main/java/com/zicna_inc/tnp_1099/service/TripServiceImpl.java
+++ src/main/java/com/zicna_inc/tnp_1099/service/TripServiceImpl.java
@@ -6,13 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zicna_inc.tnp_1099.entity.Trip;
+import com.zicna_inc.tnp_1099.entity.User;
+import com.zicna_inc.tnp_1099.entity.Gender;
+import com.zicna_inc.tnp_1099.entity.Rider;
 import com.zicna_inc.tnp_1099.repository.TripRepository;
+import com.zicna_inc.tnp_1099.repository.UserRepository;
+import com.zicna_inc.tnp_1099.repository.RiderRepository;
+import com.zicna_inc.tnp_1099.service.RiderService;
 
 @Service
 public class TripServiceImpl implements TripService{
 
     @Autowired
     TripRepository tripRepo;
+
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    RiderRepository riderRepo;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RiderService riderService;
 
     @Override
     public Trip getTrip(Long id) {
@@ -25,7 +43,14 @@ public class TripServiceImpl implements TripService{
     }
 
     @Override
-    public Trip saveTrip(Trip trip) {
+    public Trip saveTrip(Trip trip, Long user_id) {
+        // ! place to come back and fix 
+        // ! with every new trip we are creating new rider
+        Rider rider = new Rider("riderOne@example.com",Gender.MALE);
+        trip.setRider(riderService.saveRider(rider));
+        
+        User user = userService.getUser(user_id);
+        trip.setUser(user);
        return  tripRepo.save(trip);
     }
 
