4f1aa2ea5d7b1a2053055eee543b72e2d5064d4e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-03-30 13:45:27 -0500

    enable CORS for ExpenseController

--- src/main/java/com/zicna_inc/tnp_1099/web/ExpenseController.java
+++ src/main/java/com/zicna_inc/tnp_1099/web/ExpenseController.java
@@ -5,6 +5,7 @@ import com.zicna_inc.tnp_1099.service.ExpenseService;
 
 import java.util.*;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ExpenseController {
 
     @Autowired
@@ -29,24 +30,24 @@ public class ExpenseController {
     }
 
     @GetMapping("user/{user_id}/expense/all")
-    public ResponseEntity<List<Expense>> getAllUserExpenses(@PathVariable Long user_id){
+    public ResponseEntity<List<Expense>> getAllUserExpenses(@PathVariable Long user_id) {
         return new ResponseEntity<>(expenseService.getAllUserExpenses(user_id), HttpStatus.OK);
     }
 
     @GetMapping("user/{user_id}/expense/{id}")
-    public ResponseEntity<Expense> getExpense(@PathVariable Long user_id, @PathVariable Long id){
+    public ResponseEntity<Expense> getExpense(@PathVariable Long user_id, @PathVariable Long id) {
         return new ResponseEntity<>(expenseService.getExpense(user_id, id), HttpStatus.OK);
-    } 
-    
+    }
+
     @PutMapping("user/{user_id}/expense/{id}")
-    public ResponseEntity<Object> updateExpense(@PathVariable Long user_id, @PathVariable Long id, @RequestBody Expense expense){
+    public ResponseEntity<Object> updateExpense(@PathVariable Long user_id, @PathVariable Long id,
+            @RequestBody Expense expense) {
         return new ResponseEntity<>(expenseService.updateExpense(user_id, id, expense), HttpStatus.OK);
     }
 
     @DeleteMapping("user/{user_id}/expense/{id}")
-    public ResponseEntity<HttpStatus> deleteExpense(@PathVariable Long user_id, @PathVariable Long id){
+    public ResponseEntity<HttpStatus> deleteExpense(@PathVariable Long user_id, @PathVariable Long id) {
         expenseService.deleteExpense(user_id, id);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
- 
