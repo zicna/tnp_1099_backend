39ce0c21b921481f0e4a2c292e34a1c725b58e16
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-05-23 09:58:07 -0500

    add new UserReguest class to handle user requests

---
+++ src/main/java/com/zicna_inc/tnp_1099/request/UserRequest.java
@@ -0,0 +1,61 @@
+package com.zicna_inc.tnp_1099.request;
+
+import java.util.Date;
+
+import com.zicna_inc.tnp_1099.entity.Gender;
+
+public class UserRequest {
+
+    private String email;
+    private Date date_of_birth;
+    private Gender gender;
+
+    public UserRequest() {
+    }
+
+    public UserRequest(String email, Date date_of_birth, Gender gender) {
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate_of_birth() {
+        return this.date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public UserRequest email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public UserRequest date_of_birth(Date date_of_birth) {
+        setDate_of_birth(date_of_birth);
+        return this;
+    }
+
+    public UserRequest gender(Gender gender) {
+        setGender(gender);
+        return this;
+    }
+
+}
