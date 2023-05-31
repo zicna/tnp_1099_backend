eb26d054b25aafa7b47cad88a657649fb136046f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2023-05-31 14:14:14 -0500

    validate field type of Integer with combo @Min @NotNull validation annotation

--- src/main/java/com/zicna_inc/tnp_1099/request/CarRequest.java
+++ src/main/java/com/zicna_inc/tnp_1099/request/CarRequest.java
@@ -4,14 +4,15 @@ import com.zicna_inc.tnp_1099.enums.UberType;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class CarRequest {
     @NotBlank(message = "make can not be blank")
     private String make;
     @NotBlank(message = "model can not be blank")
     private String model;
-    @NotBlank(message = "year can not be blank")
-    @Min(value = 2000, message = "car can not be older than 2000")
+    @Min(value = 2000, message = "car must exist and can not be older than 2000")
+    @NotNull(message = "car must exist and can not be older than 2000")
     private Integer year;
     @NotBlank(message = "vin can not be blank")
     private String vin;
